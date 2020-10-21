89431bb602c69a3cc156581d3572a284c8ca9bd6
Abhinav Dixit <boltmillee@gmail.com>
2020-10-21 07:08:46 +0530

    1 ms

--- solutions/200. Number of Islands.java
+++ solutions/200. Number of Islands.java
@@ -1,34 +1,35 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        
-        int result=0;
-        
-        for(int i=0;i< grid.length; i++){
-            for(int j=0; j< grid[i].length; j++){
-                if(grid[i][j] == '1'){
-                    result++;
-                    dfs(grid, i, j);
-                }
-                    
-            }
-        }
-        
-        return result;
-    }
-    
-    void dfs(char[][] grid, int i, int j){
-        if(i < 0 || i> grid.length-1 || j < 0 || j> grid[i].length-1)
-            return;
-        
-        if(grid[i][j] == '1')
-            grid[i][j] = 0;
-        else
-            return;
-        
-        dfs(grid, i-1, j);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
-        
-    }
+    public int numIslands(char[][] grid) {
+        int result = 0;
+        
+        if(grid.length == 0 || grid[0].length == 0)
+            return result;
+        
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                 if(grid[i][j] == '1'){
+                     dfs(grid, i, j);
+                     result++;
+                 }
+            }
+        }
+        
+        return result;
+    }
+    
+    public void dfs(char[][] grid, int i, int j){
+        
+        if(i<0 || i >= grid.length || j<0 || j>=grid[0].length)
+            return;
+        
+        if(grid[i][j] == '1')
+            grid[i][j] = '0';
+        else
+            return;
+        
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
 }
